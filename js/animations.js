3c11c99c31ac7682e310d0e590646d8af2502564
Ryan Kramer <rkramer613@gmail.com>
2017-02-02 20:46:12 -0700

    Bam! Counts up and down!!!

--- js/animations.js
+++ js/animations.js
@@ -1,15 +1,27 @@
 
 $(document).ready(function(){
 
+var keystrokes = 5;
+
     $('.tweet-compose').on('click', function(){
         $(this).height(65);
         $('#tweet-controls').show();
-    })
+    });
 
+    $('.tweet-compose').keyup(function() {
+        var length = $(this).val().length;
+        var text = keystrokes-length;
+        $('#char-count').text(text);
 
+        if (keystrokes <= 10) {
+            $('#char-count').css('color', 'red')
+        }
 
-
-
+        if (keystrokes <= 0) {
+            $('.button').attr('disabled', 'disabled');
+        } 
+    });
 
 
 });
+
